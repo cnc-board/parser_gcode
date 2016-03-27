4f598f14762362ddb1d490d55b09abaf352c398e
gaterz <julien.maurat@free.fr>
2016-03-27 22:48:43 +0200

    signal template

---
+++ headers/SignalHandler.h
@@ -0,0 +1,20 @@
+/*
+ * SignalHandler.h
+ *
+ *  Created on: Mar 27, 2016
+ *      Author: julien
+ */
+
+#ifndef SIGNALHANDLER_H_
+#define SIGNALHANDLER_H_
+#include <signal.h>
+
+
+class SignalHandler {
+public:
+	SignalHandler();
+	virtual ~SignalHandler();
+	static void exitSignalHandler(int _ignored);
+};
+
+#endif /* SIGNALHANDLER_H_ */
---
+++ src/SignalHandler.cpp
@@ -0,0 +1,21 @@
+/*
+ * SignalHandler.cpp
+ *
+ *  Created on: Mar 27, 2016
+ *      Author: julien
+ */
+
+#include "../headers/SignalHandler.h"
+
+SignalHandler::SignalHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SignalHandler::~SignalHandler() {
+	// TODO Auto-generated destructor stub
+}
+
+void SignalHandler::exitSignalHandler(int _ignored) {
+
+}
