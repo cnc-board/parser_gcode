f68614caec7d9e4c8c79506aea66418810515f7e
gaterz <julien.maurat@free.fr>
2016-12-13 15:09:21 +0100

    Zynqcomm stub

---
+++ headers/Zynqcomm.hpp
@@ -0,0 +1,43 @@
+/*
+ * Zynqcomm.h
+ *
+ *  Created on: Mar 9, 2016
+ *      Author: julien
+ */
+
+#ifndef ZYNQCOMM_H_
+#define ZYNQCOMM_H_
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdint.h>
+#include "Accelconverter.h"
+
+#define R_0 0x00
+#define R_1 0x01
+#define R_2 0x02
+#define R_3 0x03
+#define R_control R_0
+#define bit_write_fifo 31
+#define bit_startcycle 30
+int open_driver_comm();
+
+void transmit_vector(Accel_vector& vect);
+
+void execute_fifo_list();
+
+void execute_reset_off();
+
+void send_reset_FPGA();
+
+unsigned int get_fifo_fill();
+
+int check_comm();
+
+int close_driver_comm();
+
+#endif /* ZYNQCOMM_H_ */
---
+++ src/Zynqcomm.cpp
@@ -0,0 +1,60 @@
+
+#include "../headers/Zynqcomm.hpp"
+static int comm_FD=-1;
+uint32_t* generator_addr = NULL;
+int open_driver_comm()
+{
+        comm_FD = open("/dev/mem", O_RDWR | O_SYNC);
+        if(comm_FD <0) {
+                perror("cannot open file");
+                return -1;
+        }
+        generator_addr = (uint32_t*)(mmap( 0, 4096, PROT_WRITE | PROT_READ, MAP_SHARED,comm_FD,0x43C00000)); //axi
+        if (check_comm()) {
+                perror("cannot open file");
+                return -1;
+        }
+        return 0;
+}
+
+void transmit_vector(Accel_vector& vect)
+{
+        generator_addr[R_3]=vect.Accel_y<<16 | vect.Accel_x;
+        generator_addr[R_2]=vect.Accel_a<<16 | vect.Accel_z;
+        generator_addr[R_1]=vect.epoch_stop&0xFFFFFFFF;
+        generator_addr[R_0]=(1<<bit_write_fifo)|((vect.epoch_stop>>32)&0xFFFF);
+}
+void execute_fifo_list()
+{
+        generator_addr[R_control]=(1<<bit_startcycle);
+}
+
+void execute_reset_off()
+{
+
+}
+
+void send_reset_FPGA()
+{
+
+}
+
+unsigned int get_fifo_fill()
+{
+ return generator_addr[R_3]*100/1024;
+}
+int check_comm()
+{
+        return 0;
+}
+
+int close_driver_comm()
+{
+        if(munmap(generator_addr,1) == -1) {
+                perror("cannot munmap");
+                return -1;
+        }
+        close(comm_FD);
+        comm_FD=-1;
+        return 0;
+}
